c62051338d9dccb816a12564f4789dee6a5240a8
zhuxinyu <81219723@qq.com>
2023-03-31 14:01:27 +0800

    init

---
+++ pages/demo4/demo4.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<form @submit="onSubmit">
+			<view class="row">
+				<input type="text" name="username">
+			</view>
+			<view class="row">
+				<textarea name="content"></textarea>
+			</view>
+			<view class="row">
+				<radio-group name="gender">
+					<radio value="0">女</radio>
+					<radio value="1">男</radio>
+					<radio value="2" checked>保密</radio>
+				</radio-group>
+			</view>
+			<view class="row">
+				<picker :range="options" @change="onPickerChange" name="graduated" :value="selecteValue">
+					<view>点击选择学历：{{options[selecteValue]}}</view>
+				</picker>
+
+			</view>
+			<view class="row">
+				<button form-type="submit" type="primary">提交信息</button>
+				<button form-type="reset">重置信息</button>
+			</view>
+		</form>
+		{{obj}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				obj: null,
+				options: ['高中', '大专', '本科', '硕士', '博士'],
+				selecteValue: 2
+			};
+		},
+		methods: {
+			onSubmit(e) {
+				console.log(e)
+				this.obj = e.detail.value
+			},
+			onPickerChange(e) {
+				console.log(e)
+				this.selecteValue = e.detail.value
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.row {
+		margin: 20rpx 20px;
+
+		input,
+		textarea {
+			border: 1rpx solid dodgerblue;
+			width: 100%;
+		}
+
+		button {
+			margin-top: 20rpx;
+		}
+
+		radio {
+			margin-right: 20rpx;
+		}
+	}
+</style>
